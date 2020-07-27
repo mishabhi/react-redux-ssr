b2b7c71a115536557bb3020b13cd33edb2c5eeae
Abhishek Mishra <abhishaku@hotmail.com>
2020-07-27 13:14:46 +0530

    Update api-resources.js

--- src/services/resources/api-resources.js
+++ src/services/resources/api-resources.js
@@ -1,2 +1,2 @@
-export const SERIES_URL = "https://raw.githubusercontent.com/StreamCo/react-coding-challenge/master/feed/sample.json";
-export const ALL_MOVIES = "https://raw.githubusercontent.com/StreamCo/react-coding-challenge/master/feed/sample.json";
+export const SERIES_URL = "https://raw.githubusercontent.com/mishabhi/react-redux-ssr/master/data/sample.json";
+export const ALL_MOVIES = "https://raw.githubusercontent.com/mishabhi/react-redux-ssr/master/data/sample.json";
